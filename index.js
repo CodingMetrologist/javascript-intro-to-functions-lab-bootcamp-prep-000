a247286c35febdf2633fbff9c5a7b8eff7e4348d
Chad <chad.holzkamper@gmail.com>
2018-12-16 19:54:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log('HELLO')
+  return ('hello')
+}
+
+function logWhisper(string){
+  console.log('hello')
+  return ('HELLO')
+}
+
+
+function sayHiToGrandma(string){
+  var lowercase = 'hello'
+  var uppercase = 'HELLO'
+  var mixedCase = 'I love you, Grandma.'
+  
+  if (string.toLowerCase() === lowercase){
+    console.log('hello')
+    return ("I can't hear you!")
+  }
+  else{
+    console.log('I love you, Grandma.')
+    return ('I love you, too.')
+  }
+  
+  if (string.toUpperCase() === uppercase){
+    console.log('HELLO')
+    return ('YES INDEED!')
+  }
+  else{
+    console.log('I love you, Grandma.')
+    return ('I love you, too.')
+  }
+}
+
